82716f9917c17c484c2286e422549a3792166cd1
Preciouschukwuemeka <Precious.chukwuemeka@verizon.com>
2019-07-18 16:41:06 -0500

    commit

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,11 +1,18 @@
+/*** 
+   Treehouse FSJS Techdegree 
+
+   Project 5 - Public API Requests
+***/
+
+//  NAME: Precious Chukwuemeka
 
 
+// Grabbing information for 12 random “Employees”, Using the Random User Generator API "(https://randomuser.me/)".
 
 $.ajax({
-    url: 'https://randomuser.me/api/',
-    dataType: 'json',
-    success: function(data) {
-      console.log(data);
-    }
-  });
-       
+  url: 'https://randomuser.me/api/?results=12',
+  dataType: 'json',
+  success: function (data) {
+    console.log(data);
+  }
+});
