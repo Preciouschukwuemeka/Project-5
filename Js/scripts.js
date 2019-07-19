49471df8dd6b343f5a4491ad2b824df2e5f9074c
Preciouschukwuemeka <Precious.chukwuemeka@verizon.com>
2019-07-19 14:02:11 -0500

    commit

--- Js/scripts.js
+++ Js/scripts.js
@@ -9,6 +9,9 @@
 
 // Grabbing information for 12 random “Employees”, Using the Random User Generator API "(https://randomuser.me/)".
 
+
+
+
 $.ajax({
   url: 'https://randomuser.me/api/?results=12',
   dataType: 'json',
@@ -16,3 +19,28 @@ $.ajax({
     console.log(data);
   }
 });
+
+
+
+function jasmine (){
+  let cardHTML = 
+`<div class="card">
+<div class="card-img-container">
+    <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+</div>
+<div class="card-info-container">
+    <h3 id="name" class="card-name cap">first last</h3>
+    <p class="card-text">email</p>
+    <p class="card-text cap">city, state</p>
+</div>
+</div>`;
+
+ $(".card").data.results();
+
+
+
+ }
+
+
+
+
